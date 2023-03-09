3fe40f946ada82fe9415db33fc2d2ba44b97b168
Geovanny Ortiz <geo.g.ortiz@gmail.com>
2023-03-09 15:50:15 -0500

    Fixed some errors with the program still WIP

--- gitproject.py
+++ gitproject.py
@@ -5,38 +5,43 @@ def menu():
     print("2. Decode")
     print("3. Quit")
 
+
 def encode():
     password = input("Please enter your password to encode: ")
     old_password = password
     for i in range(0, len(password)):
         int_changer = int(password[i])
         int_changer += 3
-        password[i] = str(int_changer)
+        str_int_changer = str(int_changer)
+        password = password.replace(password[i], str_int_changer)
     print("Your password has been encoded and stored!")
     print(" ")
     return password
+
 
 def decode(password):
     new_password = password
     for i in range(0, len(password)):
         int_changer = int(password[i])
         int_changer -= 3
-        password[i] = str(int_changer)
+        str_int_changer = str(int_changer)
+        password = password.replace(password[i],str_int_changer)
     print("The encoded password is " + new_password + " , and the original password is " + password + ".")
 
 
 def main():
     menu()
     password = None
-    choice = int(input("Please enter an option: "))
     program_active = True
     while program_active:
+        choice = int(input("Please enter an option: "))
         if choice == 1:
             password = encode()
         elif choice == 2:
             decode(password)
         else:
             program_active = False
+
 
 if __name__ == "__main__":
     main()
